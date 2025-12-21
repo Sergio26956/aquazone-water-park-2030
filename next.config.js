9e2a831e474d94a11f474cd22fc5c10d2b356c5c
sergio <sergiolopezantequera@gmail.com>
2025-12-21 05:39:44 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
+  compress: true,
+  images: {
+    formats: ["image/avif", "image/webp"]
   }
 }
 
