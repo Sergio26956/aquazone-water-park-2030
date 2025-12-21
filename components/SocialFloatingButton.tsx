30a03b39c4443e98fedf7de636c0549fbdafd0c4
sergio <sergiolopezantequera@gmail.com>
2025-12-21 07:19:10 +0100

    Update SocialFloatingButton.tsx

--- components/SocialFloatingButton.tsx
+++ components/SocialFloatingButton.tsx
@@ -1,1 +1,15 @@
-// placeholder file: components/SocialFloatingButton.tsx
+"use client"
+import { FaInstagram, FaFacebook, FaTiktok } from "react-icons/fa"
+
+export default function SocialFloatingButton(){
+  return (
+    <div style={{
+      position:"fixed", right:20, bottom:100, zIndex:9999,
+      display:"flex", flexDirection:"column", gap:12
+    }}>
+      <a href="#" target="_blank"><FaInstagram size={26}/></a>
+      <a href="#" target="_blank"><FaFacebook size={26}/></a>
+      <a href="#" target="_blank"><FaTiktok size={26}/></a>
+    </div>
+  )
+}
