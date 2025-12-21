744de47ed66e3b6cb869fcc03ff26fa90b6353da
sergio <sergiolopezantequera@gmail.com>
2025-12-21 05:27:40 +0100

    Update ContactList.tsx

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -1,1 +1,10 @@
-// placeholder file: components/ContactList.tsx
+"use client"
+
+export default function ContactList() {
+  return (
+    <div style={{ marginTop: 30 }}>
+      <h3>Contactos</h3>
+      <p>Preparado para base de datos y API.</p>
+    </div>
+  )
+}
