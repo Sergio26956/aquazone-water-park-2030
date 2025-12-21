1dbda13f58eb8b231eba7c9b7c0739c619ad73aa
sergio <sergiolopezantequera@gmail.com>
2025-12-21 07:07:25 +0100

    Update galeriaterrestre.tsx

--- app/galerias/galeriaterrestre.tsx
+++ app/galerias/galeriaterrestre.tsx
@@ -1,16 +1,10 @@
+"use client"
 import FuturisticGallery from "@/components/FuturisticGallery"
 
-export default function GaleriaTerrestre() {
-  return (
-    <main style={{ padding: "140px 8vw" }}>
-      <h1>Galería Terrestre</h1>
-      <FuturisticGallery
-        images={[
-          "/images/urbanos/6.jpg",
-          "/images/urbanos/7.jpg",
-          "/images/urbanos/8.jpg"
-        ]}
-      />
-    </main>
-  )
+export default function GaleriaTerrestre(){
+  return <FuturisticGallery images={[
+    "/images/urbanos/1.jpg",
+    "/images/urbanos/2.jpg",
+    "/images/urbanos/3.jpg"
+  ]}/>
 }
