47434e0a69ca00d5b436f34c338639c3acf5124f
sergio <sergiolopezantequera@gmail.com>
2025-12-21 07:18:02 +0100

    Update galeriakamikaze.tsx

--- app/galerias/galeriakamikaze.tsx
+++ app/galerias/galeriakamikaze.tsx
@@ -1,16 +1,10 @@
+"use client"
 import FuturisticGallery from "@/components/FuturisticGallery"
 
-export default function GaleriaKamikaze() {
-  return (
-    <main style={{ padding: "140px 8vw" }}>
-      <h1>Kamikaze Jump</h1>
-      <FuturisticGallery
-        images={[
-          "/images/kamikaze/1.jpg",
-          "/images/kamikaze/2.jpg",
-          "/images/kamikaze/3.jpg"
-        ]}
-      />
-    </main>
-  )
+export default function GaleriaKamikaze(){
+  return <FuturisticGallery images={[
+    "/images/kamikaze/1.jpg",
+    "/images/kamikaze/2.jpg",
+    "/images/kamikaze/3.jpg"
+  ]}/>
 }
