dd71e3d388f61d43ad0e63a533f221330d301547
sergio <sergiolopezantequera@gmail.com>
2025-12-21 05:30:06 +0100

    Update AdminPanel.tsx

--- components/AdminPanel.tsx
+++ components/AdminPanel.tsx
@@ -1,1 +1,18 @@
-// placeholder file: components/AdminPanel.tsx
+"use client"
+
+import Calendar from "./Calendar"
+import ContactList from "./ContactList"
+import Presupuestos from "./Presupuestos"
+import CampaignManager from "./CampaignManager"
+
+export default function AdminPanel() {
+  return (
+    <main style={{ padding: "140px 8vw" }}>
+      <h1>Panel Administrativo</h1>
+      <Calendar />
+      <ContactList />
+      <Presupuestos />
+      <CampaignManager />
+    </main>
+  )
+}
