e712a604e96549460f46f324ca8c65dfc5da05bd
sergio <sergiolopezantequera@gmail.com>
2025-12-21 05:17:59 +0100

    Update BudgetForm.tsx

--- components/BudgetForm.tsx
+++ components/BudgetForm.tsx
@@ -1,1 +1,10 @@
-// placeholder file: components/BudgetForm.tsx
+"use client"
+
+export default function BudgetForm() {
+  return (
+    <div>
+      <h3>Solicitud de presupuesto</h3>
+      <p>Integrado con IA y email.</p>
+    </div>
+  )
+}
