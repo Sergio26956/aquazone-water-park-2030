5603012a6b32d748eca6575b477ca2266830a2b5
sergio <sergiolopezantequera@gmail.com>
2025-12-23 07:03:47 +0100

    Update page.tsx

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -2,18 +2,12 @@ import ContactForm from "@/components/ContactForm";
 import BudgetForm from "@/components/BudgetForm";
 import SectionTransition from "@/components/SectionTransition";
 
-export default function ContactPage() {
+export default function Contacto() {
   return (
-    <div style={{ padding: "50px", display: "flex", flexDirection: "column", gap: "50px" }}>
-      <h2 style={{ textAlign: "center", color: "#32CD32" }}>Contacto</h2>
-      <SectionTransition>
-        <ContactForm />
-      </SectionTransition>
-
-      <h2 style={{ textAlign: "center", color: "#FFD700" }}>Solicitar Presupuesto</h2>
-      <SectionTransition>
-        <BudgetForm />
-      </SectionTransition>
-    </div>
+    <SectionTransition>
+      <h1>Contacto</h1>
+      <ContactForm />
+      <BudgetForm />
+    </SectionTransition>
   );
 }
