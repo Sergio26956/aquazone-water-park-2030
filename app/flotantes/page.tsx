87a592702097fd422a2ad8ee8b99ed18c122f359
sergio <sergiolopezantequera@gmail.com>
2025-12-23 08:07:40 +0100

    Update page.tsx

--- app/flotantes/page.tsx
+++ app/flotantes/page.tsx
@@ -1,24 +1,11 @@
-import SectionTransition from "@/components/SectionTransition";
-import FuturisticGallery from "@/components/FuturisticGallery";
+"use client";
+
+import SectionTransition from "../../components/SectionTransition";
 
 export default function FlotantesPage() {
-  const images = [
-    "/images/flotantes/piscina1.jpg",
-    "/images/flotantes/playa1.jpg",
-    "/images/flotantes/piscina2.jpg"
-  ];
-
   return (
-    <div style={{ padding: "50px" }}>
-      <h2 style={{ textAlign: "center", color: "#00BFFF" }}>Parques Flotantes Modulares</h2>
-      <SectionTransition>
-        <p style={{ textAlign: "center", fontSize: "1.2rem" }}>
-          Piscinas y playas modulares flotantes con diseño futurista 2030, totalmente seguros y personalizables.
-        </p>
-      </SectionTransition>
-      <SectionTransition>
-        <FuturisticGallery images={images} />
-      </SectionTransition>
-    </div>
+    <SectionTransition>
+      <h2>Parques Acuáticos Flotantes</h2>
+    </SectionTransition>
   );
 }
