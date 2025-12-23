128ccdaeb7078da309bf943c009399a1c6884c16
sergio <sergiolopezantequera@gmail.com>
2025-12-23 08:04:37 +0100

    Update page.tsx

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,11 +1,12 @@
-import ContactForm from "@/components/ContactForm";
-import BudgetForm from "@/components/BudgetForm";
-import SectionTransition from "@/components/SectionTransition";
+"use client";
 
-export default function Contacto() {
+import ContactForm from "../../components/ContactForm";
+import BudgetForm from "../../components/BudgetForm";
+import SectionTransition from "../../components/SectionTransition";
+
+export default function ContactoPage() {
   return (
     <SectionTransition>
-      <h1>Contacto</h1>
       <ContactForm />
       <BudgetForm />
     </SectionTransition>
