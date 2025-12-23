57409262ca9bd6a24d80c0c3781ff6f84234063b
sergio <sergiolopezantequera@gmail.com>
2025-12-23 10:03:05 +0100

    Update page.tsx

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import ContactForm from "../../components/ContactForm";
-import BudgetForm from "../../components/BudgetForm";
-import SectionTransition from "../../components/SectionTransition";
+import ContactForm from "@/components/ContactForm";
+import BudgetForm from "@/components/BudgetForm";
+import SectionTransition from "@/components/SectionTransition";
 
 export default function ContactoPage() {
   return (
-    <SectionTransition>
+    <main>
+      <SectionTransition />
       <ContactForm />
       <BudgetForm />
-    </SectionTransition>
+    </main>
   );
 }
